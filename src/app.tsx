643fec35caca101c9b8d7daee47248ec874dd285
Tarun Garg <t.garg28@gmail.com>
2024-04-06 06:33:28 +0530

    2nd cmmit

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
-import React, { Suspense, lazy } from 'lib/react';
+import React, { Suspense, lazy } from "lib/react";
 
-import type AccuralProps from 'application-a/types/AccuralInterface';
+import type AccuralProps from "application-a/types/AccuralInterface";
 
-const Accural = lazy(() => import('billingApp/accural')) as unknown as typeof AccuralProps;
+const Accural = lazy(
+  () => import("billingApp/accural")
+) as unknown as typeof AccuralProps;
 
 const App = () => {
   return (
     <ul>
       bbbbbbbbbbbb
       <Suspense fallback="Loading Button">
-        <Accural a={'aaaaaaaaaaaaaaaaa'} b={'bbbbbbbbbbbbbb'} />
+        <Accural a={"aaaaaaaaaaaaaaaaa"} b={"bbbbbbbbbbbbbb"} />
       </Suspense>
     </ul>
   );
